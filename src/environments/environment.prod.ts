57066765a95c38f576923d43bc24699f27e03d77
Allison Nunes <allison.nunes@maxmilhas.com.br>
2022-12-29 19:10:58 -0300

    Change endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api-sigga.herokuapp.com"
+  apiUrl: "https://generic-api-json-server.vercel.app"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://api-sigga.herokuapp.com"
+  apiUrl: "http://localhost:3000"
 };
 
 /*
