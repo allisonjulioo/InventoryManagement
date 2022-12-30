b858c775128a2a55571a68e1630c9d0c2e986eba
Allison Nunes <allison.nunes@maxmilhas.com.br>
2022-12-29 22:48:25 -0300

    Adjust code smell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { InventoryComponent } from './pages/inventory/inventory.component';
 import { ReceivementComponent } from './pages/receivement/receivement.component';
 
-
 const routes: Routes = [
-  { path: "inventory", component: InventoryComponent, pathMatch: "full", data: { title: 'Inventario' } },
-  { path: "received/:id", component: ReceivementComponent, pathMatch: "full", data: { title: "Recebimento" } },
-  { path: "**", redirectTo: 'inventory' },
+  {
+    path: 'inventory',
+    component: InventoryComponent,
+    pathMatch: 'full',
+    data: { title: 'Inventario' },
+  },
+  {
+    path: 'received/:id',
+    component: ReceivementComponent,
+    pathMatch: 'full',
+    data: { title: 'Recebimento' },
+  },
+  { path: '**', redirectTo: 'inventory' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true )],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
